13fe7e07953b1730e5618e4cc3f823f39ddf4401
Arpit Jaiswal <arpitjaiswalaj9@gmail.com>
2024-11-17 14:39:39 +0530

    update again

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,18 @@ package com.driver;
 public class Main {
     public static  void main(String[] args)
     {
-        B b = new B();
-//        System.out.println(b.meth());
-        b.meth();
-        b.meth();
-//        System.out.println(b.meth());
+        B objB = new B();
+        System.out.println(objB.meth()); // Calls method from class A
+
+        // Task 4: Override method meth of class A in class B
+        B objBOverridden = new B() {
+            @Override
+            public String meth() {
+                return "Method is overridden in Extended class B";
+            }
+        };
+
+        // Task 5: Call this overridden method from object of class B
+        System.out.println(objBOverridden.meth());
     }
 }
