079aabf0663f19efbcf1c32db7c33b1f26b79f7f
Arpit Jaiswal <arpitjaiswalaj9@gmail.com>
2024-11-17 14:34:41 +0530

    update the assighment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static  void main(String[] args)
     {
         B b = new B();
-        System.out.println(b.meth());
-        System.out.println(b.meth());
+//        System.out.println(b.meth());
+        b.meth();
+        b.meth();
+//        System.out.println(b.meth());
     }
 }
